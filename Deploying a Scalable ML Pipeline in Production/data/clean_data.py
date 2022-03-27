d1e7afc931ce64450abf7519b53bc0653f00faf0
Hyacinth Ampadu <ampaduh@gmail.com>
2022-03-27 21:25:41 +0000

    Add files via upload

---
+++ Deploying a Scalable ML Pipeline in Production/data/clean_data.py
@@ -0,0 +1,31 @@
+import logging
+import pandas as pd
+
+
+def load_data(path):
+    try:
+        df = pd.read_csv(path)
+        logging.info('SUCCESS: Data succesfully imported')
+        return df
+    except BaseException:
+        logging.info('ERROR: Data not imported')
+
+
+def cleaned_data(df):
+    try:
+        df.columns = df.columns.str.strip()
+        df['salary'] = df['salary'].str.lstrip()
+        df.drop("fnlgt", axis="columns", inplace=True)
+        df.drop("education-num", axis="columns", inplace=True)
+        df.drop("capital-gain", axis="columns", inplace=True)
+        df.drop("capital-loss", axis="columns", inplace=True)
+        df.to_csv('census_cleaned.csv')
+        logging.info('SUCCESS:Data cleaned!')
+        return df
+    except BaseException:
+        logging.info('ERROR Data could not be cleaned')
+
+
+if __name__ == '__main__':
+    df = load_data("census.csv")
+    cleaned_data(df)
